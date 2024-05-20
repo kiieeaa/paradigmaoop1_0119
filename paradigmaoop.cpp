e543b1c93032933fa498de6593f49ca90f39f59a
kiieeaa <kiyaadinda5@gmail.com>
2024-05-20 10:29:22 +0700

    mahasiswa

--- paradigmaoop.cpp
+++ paradigmaoop.cpp
@@ -10,6 +10,10 @@ private:
 	float nilaii;
 
 public:
-	mahasiswa(int pId,string pNama, float pNilai) :id(pId), nama(pNama)
+	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama)
 		//definisis
+}
 
+~mahasiswa() {
+
+}
