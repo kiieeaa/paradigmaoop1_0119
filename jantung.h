c304da8fa765a8c7f8b0e4d0701d999cf6050c3e
kiieeaa <kiyaadinda5@gmail.com>
2024-05-20 10:56:39 +0700

    using namespace

--- jantung.h
+++ jantung.h
@@ -9,6 +9,10 @@ public:
 	jantung() {
 		cout << "jantung dihidupkan\n";
 	}
-	~
+	~jantung() {
+		cout << "jantung dimatikan\n";
+	}
+};
+#endif
 
 
---
+++ manusia.h
@@ -0,0 +1,4 @@
+#pragma once
+#ifndef MANUSIA_H
+#define MANUSIA_H
+using namespace
