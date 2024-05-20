d86233403e7105920cca9c01a3aff72ec7e84b2f
kiieeaa <kiyaadinda5@gmail.com>
2024-05-20 14:50:17 +0700

    anak

---
+++ anakk.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef ANAK_H
+#define ANAK_H
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class anak {
+public:
+	string nama;
+	anak(string pNama) : nama(pNama) {
+		cout << "Anak \"" << nama << "\" ada\n";
+	}
+	~anak() {
+		cout << "Anak \ "" << nama << \" tidak ada \n";
+	}
+};
+#endif
